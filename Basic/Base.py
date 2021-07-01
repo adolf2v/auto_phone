66bd35b72f3515f01ddc5d24b09ba4a5b673fcbb
xiaoqiang <weiqiang.liu@qunar.com>
2021-07-01 21:55:35 +0800

    update find_element func

--- Basic/Base.py
+++ Basic/Base.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
 
 
 class Base(object):
@@ -6,8 +7,8 @@ class Base(object):
         self.driver = driver
 
     def find_element(self, loc, timeout=1):
-        print('第二步')
-        return WebDriverWait(self.driver, timeout).until(lambda x: x.find_element(*loc))
+        print('第二步', loc)
+        return WebDriverWait(self.driver, timeout).until(lambda x: x.find_element(by=By.ID, value=loc))
 
     def click_element(self, loc):
         self.find_element(loc).click()
